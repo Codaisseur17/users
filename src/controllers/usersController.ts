999dfa245dbd4e76c596a3737a1e5c9dfc4294b7
adapass182 <adampassingham90@gmail.com>
2018-06-06 13:56:04 +0200

    added get all users, added password hashing to users controller

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,22 +1,34 @@
-import { JsonController, Get, Post, Param, Body, HttpCode } from 'routing-controllers'
+import { JsonController, Get, Post, Param, Body, HttpCode, NotFoundError } from 'routing-controllers'
 import User from '../entities/users'
 
 @JsonController()
 export default class UsersController {
     
+    @Get('/users')
+    async getAllUsers()
+    {
+        const allUsers = await User.find()
+        if (!allUsers) throw new NotFoundError('Cannot find users table')
+        return {allUsers}
+    }
+
     @Get('/users/:id')
-    async user(
+    async getUser(
         @Param('id') id: number
     ) {
-        return User.findOne(id)
+        const user = await User.findOne(id)
+        return {user}
     }
 
     @Post('/users')
     @HttpCode(201)
-    addUser(
+    async addUser(
         @Body() newUser: User
     ) {
-        return newUser.save()
+        const {password, ...x} = newUser
+        const userEntity = User.create(x)
+        await userEntity.hashPassword(password)
+        return userEntity.save()
     }
 
 }
